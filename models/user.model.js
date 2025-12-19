e5b2a4518ef00d429f573297ff7087cb8a8f1ac5
Vasuki t <iamvasukithangaraj@gmail.com>
2025-12-19 21:29:59 +0530

    created user model

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+  },
+});
+const users = new mongoose.model("users", UserSchema);
+module.exports = users;
