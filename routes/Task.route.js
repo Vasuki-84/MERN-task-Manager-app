d1c5b38788d3115d4784a3abe37b02186b6d7fe5
Vasuki t <iamvasukithangaraj@gmail.com>
2025-12-20 18:17:28 +0530

    added authorization

--- routes/Task.route.js
+++ routes/Task.route.js
@@ -8,8 +8,10 @@ const {
   deleteTask,
 } = require("../controllers/Task.controller");
 
+const authMiddleware = require("../middleware/auth.middleware");
+
 // http://localhost:8081/tasks/create
-router.post("/create", createTasks);
+router.post("/create", authMiddleware(["admin"]), createTasks);
 
 // http://localhost:8081/tasks
 router.get("/", getAllTasks);
@@ -18,9 +20,9 @@ router.get("/", getAllTasks);
 router.get("/:id", getTasksById);
 
 //  http://localhost:8081/tasks/update/<id>
-router.put("/update/:id", updateTasks);
+router.put("/update/:id", authMiddleware(["admin"]), updateTasks);
 
 //  http://localhost:8081/tasks/delete/<id>
-router.delete("/delete/:id", deleteTask);
+router.delete("/delete/:id", authMiddleware(["admin"]), deleteTask);
 
 module.exports = router;
